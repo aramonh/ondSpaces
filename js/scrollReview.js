60fe0cfddbf0c79bf2d8da975e520aff26354173
aramonh <43154910+aramonh@users.noreply.github.com>
2020-02-20 22:54:49 -0500

    Add files via upload

---
+++ js/scrollReview.js
@@ -0,0 +1,112 @@
+
+
+window.sr = ScrollReveal();
+//MAIN
+sr.reveal('#logoMain1',{
+    duration:4000,
+    origin: 'top',
+    delay:1000
+})
+window.sr = ScrollReveal();
+sr.reveal('#btnMain1',{
+    duration:2000,
+    origin: 'bottom',
+    distance:'50px'
+})
+window.sr = ScrollReveal();
+sr.reveal('#btnMain2',{
+    duration:1500,
+    origin: 'bottom',
+    distance:'50px'
+})
+window.sr = ScrollReveal();
+sr.reveal('#btnMain3',{
+    duration:1000,
+    origin: 'bottom',
+    distance:'50px'
+})
+window.sr = ScrollReveal();
+sr.reveal('#videoMain1',{
+    duration:3000,
+    origin: 'top', 
+})
+window.sr = ScrollReveal();
+sr.reveal('#footerMain',{
+    duration:6000,
+    origin: 'top', 
+})
+//HOME
+window.sr = ScrollReveal();
+sr.reveal('nav',{
+    duration:3000,
+    origin: 'top', 
+    distance:'100px'
+})
+
+sr.reveal('#logoHome1',{
+    duration:4000,
+    origin: 'left', 
+    distance:'300px'
+})
+
+
+sr.reveal('#textIntroHome',{
+    duration:3000,
+    origin: 'rigth', 
+    delay:1000,
+    distance:'100px'
+})
+sr.reveal('.header-content-right',{
+    duration:1000,
+    origin: 'rigth', 
+    delay:2500,
+   
+})
+
+
+sr.reveal('#videoPresentation',{
+    duration:3000,
+    origin: 'bottom', 
+    distance:'100px',
+    delay:2000
+})
+
+
+
+sr.reveal('#testimonial',{
+    duration:3000,
+    origin: 'left', 
+    distance:'1500px',
+    delay:500
+})
+sr.reveal('#testimonial-text',{
+    duration:1000,
+    origin: 'left', 
+    delay:2000
+})
+
+
+sr.reveal('#textAboutUsHome',{
+    duration:2000,
+    origin: 'rigth',
+    distance:'500px'
+})
+sr.reveal('.about-content-right',{
+    duration:1500,
+    origin: 'rigth',
+    delay:2000
+})
+sr.reveal('#imageAboutUs',{
+    duration:3500,
+    origin: 'left', 
+    distance:'200px'
+})
+
+
+
+
+sr.reveal('#contact-content',{
+    duration:5000,
+    origin: 'bottom', 
+    
+})
