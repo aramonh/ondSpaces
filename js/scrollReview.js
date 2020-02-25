139331620a49706fe7f6624c39becba2e623b831
aramonh <aramonh@unal.edu.co>
2020-02-25 17:40:18 -0500

    changes texts

--- js/scrollReview.js
+++ js/scrollReview.js
@@ -7,36 +7,36 @@ sr.reveal('#logoMain1',{
     origin: 'top',
     delay:1000
 })
-window.sr = ScrollReveal();
+
 sr.reveal('#btnMain1',{
     duration:2000,
     origin: 'bottom',
     distance:'50px'
 })
-window.sr = ScrollReveal();
+
 sr.reveal('#btnMain2',{
     duration:1500,
     origin: 'bottom',
     distance:'50px'
 })
-window.sr = ScrollReveal();
+
 sr.reveal('#btnMain3',{
     duration:1000,
     origin: 'bottom',
     distance:'50px'
 })
-window.sr = ScrollReveal();
+
 sr.reveal('#videoMain1',{
     duration:3000,
     origin: 'top', 
 })
-window.sr = ScrollReveal();
+
 sr.reveal('#footerMain',{
     duration:6000,
     origin: 'top', 
 })
 //HOME
-window.sr = ScrollReveal();
+
 sr.reveal('nav',{
     duration:3000,
     origin: 'top', 
@@ -48,11 +48,16 @@ sr.reveal('#logoHome1',{
     origin: 'left', 
     distance:'300px'
 })
+sr.reveal('#textLogoHome1',{
+    duration:1000,
+    origin: 'left', 
+    delay:3000
+})
 
 
 sr.reveal('#textIntroHome',{
     duration:3000,
-    origin: 'rigth', 
+    origin: 'bottom', 
     delay:1000,
     distance:'100px'
 })
@@ -67,7 +72,7 @@ sr.reveal('.header-content-right',{
 sr.reveal('#videoPresentation',{
     duration:3000,
     origin: 'bottom', 
-    distance:'100px',
+   
     delay:2000
 })
 
@@ -88,8 +93,8 @@ sr.reveal('#testimonial-text',{
 
 sr.reveal('#textAboutUsHome',{
     duration:2000,
-    origin: 'rigth',
-    distance:'500px'
+    origin: 'bottom',
+    distance:'100px'
 })
 sr.reveal('.about-content-right',{
     duration:1500,
@@ -99,10 +104,33 @@ sr.reveal('.about-content-right',{
 sr.reveal('#imageAboutUs',{
     duration:3500,
     origin: 'left', 
-    distance:'200px'
+    distance:'100px'
 })
 
 
+
+
+sr.reveal('#textWorkWithUsHome',{
+    duration:2000,
+    origin: 'left',
+    distance:'100px'
+})
+sr.reveal('.work-with-us-content-left',{
+    duration:1500,
+    origin: 'rigth',
+    delay:2000
+})
+sr.reveal('#imageWorkWithUs',{
+    duration:3500,
+    origin: 'bottom', 
+    distance:'100px'
+})
+sr.reveal('#btnReadMoreWorkWithUs',{
+    duration:2000,
+    origin: 'bottom',
+    distance:'100px',
+    delay:2000
+})
 
 
 sr.reveal('#contact-content',{
